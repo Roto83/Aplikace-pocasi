1e2bed463a6a8214a05b24317b9cdeda98df4075
Frýba Marian <fryba.ma.2021@skola.ssps.cz>
2024-04-22 16:26:12 +0200

    further setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,37 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <section id="leftSection">
 
-  <div class="card">
-    <Counter />
-  </div>
+  </section>
+  <section id="centerSection">
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  </section>
+  <section id="rightSection">
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  </section>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  #leftSection{
+    background-color: aqua;
+    width: 15%;
+    height: 100px;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  #centerSection{
+    background-color: whitesmoke;
+    width: 60%;
+    height: 100px;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  #rightSection{
+    background-color:cornflowerblue;
+    width: 25%;
+    height: 100px;
   }
-  .read-the-docs {
-    color: #888;
+  main{
+    display: flex;
+    flex-direction: row;
   }
 </style>
