c59a7a481fe150a3b2d82485d71eaf302e290c1f
Marian Frýba <fryba.ma.2021@skola.ssps.cz>
2024-06-02 23:31:59 +0200

    HTTPS

--- src/locationFetch.js
+++ src/locationFetch.js
@@ -1,7 +1,7 @@
 async function LocationFetch(cityName, countryCode) {
     if (arguments.length > 0){
         try {
-            const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},,${countryCode}&limit=5&appid=134172c2ea1a234d8cdf9065571deedd`)
+            const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName},,${countryCode}&limit=5&appid=134172c2ea1a234d8cdf9065571deedd`)
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
